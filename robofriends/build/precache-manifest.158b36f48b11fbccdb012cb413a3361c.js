0ad2a098c5ff420a4b7c9f20dd946af97b08f253
jlayog <35618625+jlayog@users.noreply.github.com>
2019-01-31 18:37:35 -0500

    Added Build files
    removed gitignore

---
+++ robofriends/build/precache-manifest.158b36f48b11fbccdb012cb413a3361c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3daea446fad638cd8a55",
+    "url": "/static/css/main.698aa27e.chunk.css"
+  },
+  {
+    "revision": "3daea446fad638cd8a55",
+    "url": "/static/js/main.3daea446.chunk.js"
+  },
+  {
+    "revision": "588e37a539d1541e7ab5",
+    "url": "/static/css/1.02e9e099.chunk.css"
+  },
+  {
+    "revision": "588e37a539d1541e7ab5",
+    "url": "/static/js/1.588e37a5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "18d0c1f709308a9fd1b04d3efc36a27a",
+    "url": "/index.html"
+  }
+];
