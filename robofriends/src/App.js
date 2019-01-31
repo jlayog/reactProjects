21fb92bdeb51ccfa388ebbb7b7797c9f201e8b96
jlayog <35618625+jlayog@users.noreply.github.com>
2019-01-31 17:31:22 -0500

    fixed missing node modules
    added important packages for launch

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -8,13 +8,15 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
     componentDidMount() {
-        this.setState({ robots: robots });
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
